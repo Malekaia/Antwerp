29930d6c848c9314a99868b508d747ee996d86c1
LogicalBranch <>
2022-07-16 17:24:12 +0100

    document and standardise core library

--- src/core.rs
+++ src/core.rs
@@ -1,112 +1,190 @@
 #[allow(non_snake_case)]
 pub mod Antwerp {
-  use crate::lib::{file_system, walk};
+  use crate::lib;
   use colored::Colorize;
   use regex::Regex;
   use tera::{Context, Tera};
 
+
+  /// **Description**:
+  ///
+  /// Log a colourised string about a certain action
   fn print(color: &str, action: &str, category: &str, target: &str) {
     println!("{} ({}): {}", format!("{action}").color(color), format!("{category}").bold(), target);
   }
 
+
+  /// **Description**:
+  ///
+  /// Empty the root folder (see `lib::empty_dir`)
   pub fn empty_root(root: &str) {
-    {
-      print("red", "Empty", "folder", root);
-    }
-    file_system::empty_dir(root);
+    // Log the update
+    print("red", "Empty", "folder", root);
+    // Empty the directory
+    lib::empty_dir(root);
   }
 
+
+  /// **Description**:
+  ///
+  /// Used to define template information in a template group, defines the output file and the context
   pub struct Template<'a> {
     pub output: &'a str,
     pub context: Context
   }
 
+
+  /// **Description**:
+  ///
+  /// Returns a new instance of Tera and builds it's inheritance chains
   pub fn tera(template_directory: &str) -> Tera {
+    // Try to create the tera instance
     let mut tera: Tera = match Tera::parse(template_directory) {
       Ok(tera_object) => tera_object,
       Err(error) => panic!("Error: failed to render \"tera\" template:\n\n{}", error)
     };
+    // Try to build the inheritance chains
     tera.build_inheritance_chains().expect("Error: failed to build tera inheritance chain");
     tera
   }
 
+
+  /// **Description**:
+  ///
+  /// Renders a Tera template using a given context and writes it an output file
   pub fn render(tera: &Tera, template_path: &str, output: &str, context: Context) {
+    // Try to render the template
     let result: String = match tera.render(template_path, &context) {
       Ok(result) => result,
       Err(error) => panic!("Error: failed to render tera template:\n\n{}", error)
     };
-    file_system::write_file(output, &result);
+    // Write the rendered template to the output file
+    lib::write_file(output, &result);
   }
 
-  pub fn render_string(tera: &mut Tera, template: &String, context: Context) -> String {
-    match tera.render_str(&template, &context) {
+
+  /// **Description**:
+  ///
+  /// Takes a string containing a Tera template string and returns a string containing the rendered template
+  pub fn render_string(tera: &mut Tera, template_string: &String, context: Context) -> String {
+    // Try to render the string and return it (panic on fail)
+    match tera.render_str(&template_string, &context) {
       Ok(result) => result,
       Err(error) => panic!("Error: failed to render tera string:\n\n{}", error)
     }
   }
 
-  pub fn route(tera: &Tera, template: &str, output: &str, context: Context) {
-    {
-      print("green", "Render", "static", &output);
-    }
-    render(tera, template, output, context);
+
+  /// **Description**:
+  ///
+  /// Takes template information (template name, output and context) and renders it to a file
+  pub fn route(tera: &Tera, template_name: &str, output: &str, context: Context) {
+    // Log the update
+    print("green", "Render", "static", &output);
+    // Render the given template
+    self::render(tera, template_name, output, context);
   }
 
-  pub fn route_group(tera: &Tera, template_path: &str, templates: Vec<Template>) {
+
+  /// **Description**:
+  ///
+  /// Takes a Vector containing Template(s) (`Vec<Template>`) and passes each template to `lib::route`
+  pub fn route_group(tera: &Tera, template_name: &str, templates: Vec<Template>) {
     for template in templates {
-      {
-        print("green", "Render", "static", &template.output);
-      }
-      render(tera, template_path, template.output, template.context);
+      // Log the update
+      print("green", "Render", "static", &template.output);
+      // Render the current template
+      self::render(tera, template_name, template.output, template.context);
     }
   }
 
+
+  /// **Description**:
+  ///
+  /// An enum used to define data for `File`, `Folder`, and `Scss` assets for `Antwerp::assets([...])`.
+  ///
+  /// **Usage**:
+  ///
+  /// `Asset::File(source: &str, destination: &str, overwrite: bool)`
+  ///
+  /// `Asset::Folder(source: &str, destination: &str, check: &str, overwrite: bool)`
+  ///
+  /// `Asset::Scss(source: &str, destination &str)`
   pub enum Asset<'a> {
+    // source, destination, overwrite
     File(&'a str, &'a str, bool),
+    // source, destination, check, overwrite
     Folder(&'a str, &'a str, &'a str, bool),
+    // source, destination
     Scss(&'a str, &'a str),
   }
 
+
+  /// **Description**:
+  ///
+  /// Takes asset definitions using a `Vec<Asset>` and uses match to determine how to copy or compile those assets to their target destination
+  ///
+  /// **Behaviour**:
+  ///
+  /// * Copies individual files to a specified file path
+  /// * Copies files in specified folders that pass a check
+  /// * Compiles SCSS stylesheets to a specified file path
+  ///
+  /// **References**:
+  ///
+  /// * [Match (Flow Control)](https://doc.rust-lang.org/rust-by-example/flow_control/match.html)
   pub fn assets(assets: Vec<Asset>) {
     for asset in assets {
+      // Handle different asset types
       match asset {
-        // copy files
+        // Copy static assets
         Asset::File(source, destination, overwrite) => {
-          {
-            let overwrite_status: &str = if overwrite == true { "overwrite: false" } else { "overwrite: true" };
-            print("blue", "Copy", overwrite_status, &destination);
-          }
-          file_system::copy_file(&source, &destination, overwrite);
+          // Log the update
+          let overwrite_status: &str = if overwrite == true { "overwrite: false" } else { "overwrite: true" };
+          print("blue", "Copy", overwrite_status, &destination);
+
+          // Copy the file to the intended destination
+          lib::copy_file(&source, &destination, overwrite);
         },
 
-        // copy (recursive) directories
+        // Copy (recursive) directories
         Asset::Folder(source, mut destination, check, overwrite) => {
+          // Regular expression used to remove the root of the source path
           let re_root: Regex = Regex::new(r"^(.*?)/(.*?)$").unwrap();
-          let re_check: Regex = Regex::new(&check).expect(&format!("Error: failed to create regex: {}", &check));
-          for path in walk::dir(source) {
+          // Regular expression used to check the matches
+          let re_check: Regex = Regex::new(check).expect(&format!("Error: failed to create regex: {}", check));
+
+          for path in lib::walk_dir(source) {
+            // Ignore the match if it doesn't pass the check
             if !re_check.is_match(&path) {
               continue;
             }
-            destination = if destination.ends_with("/") { &destination[0..(destination.len() - 1)] } else { destination };
-            let dest: String = re_root.replace(&*path, format!("{}/$2", destination)).to_string();
-            {
-              let overwrite_status: &str = if overwrite == true { "overwrite: false" } else { "overwrite: true" };
-              print("blue", "Copy", overwrite_status, &dest);
-            }
-            file_system::copy_file(&path, &dest, overwrite);
+            // Remove trailing slashes from the destination path
+            if destination.ends_with("/") {
+              destination = &destination[0..(destination.len() - 1)]
+            };
+            // Create the destination path (without the source root)
+            let new_destination: &str = &*re_root.replace(&path, format!("{destination}/$2")).to_string();
+            // Log the update
+            let overwrite_status: &str = if overwrite == true { "overwrite: false" } else { "overwrite: true" };
+            print("blue", "Copy", overwrite_status, new_destination);
+            // Copy the file
+            lib::copy_file(&path, new_destination, overwrite);
           }
+
         },
 
-        // compile SCSS assets
+        // Compile SCSS assets
         Asset::Scss(source, destination) => {
+          // Try to compile the SCSS stylesheet (return if possible, panic if not)
           let result: String = match grass::from_path(source, &grass::Options::default()) {
             Ok(result) => result,
             Err(error) => panic!("Error: failed to compile SASS stylesheets\n\n{:?}", error)
           };
-          {
-            print("magenta", "Compile", "SCSS", &destination);
-          }
-          file_system::write_file(&destination, &result);
+          // Log the update
+          print("magenta", "Compile", "SCSS", &destination);
+          // Write the compiled SCSS to the destination file
+          lib::write_file(&destination, &result);
         }
       }
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,83 +1,222 @@
-pub mod file_system {
-  use std::{fs, fs::File, path::Path, io::prelude::Write};
+//! Contains functions used to standardise the implementation of common utilities.
+//!
+//! Provides detailed error handling and prevents code duplication.
 
-  pub fn read_file(file_path: &str) -> String {
-    fs::read_to_string(file_path).expect(&format!("Error: failed to read file: \"{}\"", file_path))
-  }
+use glob::{glob, GlobError};
+use regex::Regex;
+use std::{fs, fs::File, path::Path, io::prelude::Write, path::PathBuf};
 
-  pub fn write_file(path: &str, content: &String) {
-    // ensure the directory
-    let mut dir: Vec<&str> = path.split("/").collect::<Vec<&str>>();
-    dir.pop();
-    self::ensure_dir(&dir.join("/"));
-    // write the file
-    let error_message: &str = &format!("Error: failed to write to \"{}\"", &path);
-    let mut file: File = File::create(path).expect(error_message);
-    file.write_all(content.as_bytes()).expect(error_message);
-  }
 
-  pub fn copy_file(from: &str, to: &str, overwrite: bool) {
-    let mut to_path: Vec<&str> = to.split("/").collect::<Vec<&str>>();
-    to_path.pop();
-    self::ensure_dir(&to_path.join("/"));
-    if overwrite == true || !self::exists(to) {
-      match fs::copy(from, to) {
-        Ok(_) => {},
-        Err(error) => panic!("Error: failed copy of \"{}\" to \"{}\"\n\n{}", from, to, error)
-      }
+/// **Description**:
+///
+/// Checks if a given directory exists, shorthand for `Path::new(path).exists()`
+///
+/// **References**:
+/// * [Path::new(...).exists()](https://doc.rust-lang.org/stable/std/path/struct.Path.html#method.exists)
+pub fn exists(path: &str) -> bool {
+  Path::new(path).exists()
+}
+
+
+/// **Description**:
+///
+/// Read content from a file
+///
+/// **Behaviour**:
+/// * Open the file in read only mode
+/// * Read and return it's contents
+/// * Exit on success, panic on fail
+///
+/// **References**:
+/// * [fs::read_to_string](https://doc.rust-lang.org/std/fs/fn.read_to_string.html)
+pub fn read_file(file_path: &str) -> String {
+  fs::read_to_string(file_path).expect(&format!("Error: failed to read file: \"{}\"", file_path))
+}
+
+
+/// **Description**:
+///
+/// Write a content string to a file
+///
+/// **Behaviour**:
+/// * Open the file in write only mode
+/// * Create it if it doesn't exist and truncate if it does
+/// * Exit on success, panic on fail
+///
+/// **References**:
+/// * [io::Write](https://doc.rust-lang.org/std/io/trait.Write.html#method.write_all)
+pub fn write_file(path: &str, content: &String) {
+  // Ensure the file's directory exists
+  self::ensure_dir(&path[0..path.rfind('/').unwrap()]);
+  // Create the write error message
+  let write_error: &str = &format!("Error: failed to write file \"{}\"", &path);
+  // Opens the file in write-only mode
+  // Create the file if it doesn't exist / truncate it if it does
+  let mut file: File = File::create(path).expect(write_error);
+  // Write to the file
+  file.write_all(content.as_bytes()).expect(write_error);
+}
+
+
+/// **Description**:
+///
+/// Copy a file from a source to a destination, with explicit overwrite permissions
+///
+/// **Behaviour**:
+/// * Ensure the destination directory exists using `lib::ensure`
+/// * Only write to the file if it doesn't already exist or if `overwrite` is `true`
+/// * Exit on success, panic on fail
+///
+/// **References**:
+/// * [fs::copy](https://doc.rust-lang.org/std/fs/fn.copy.html)
+pub fn copy_file(from: &str, to: &str, overwrite: bool) {
+  // Ensure the directory of the copy destination exists
+  self::ensure_dir(&to[0..to.rfind('/').unwrap()]);
+  // Write to the file if it doesn't exist or if overwrite is enabled
+  if !self::exists(to) || overwrite == true {
+    match fs::copy(from, to) {
+      Ok(_) => { /* do nothing on success */ },
+      Err(error) => panic!("Error: failed copy of \"{}\" to \"{}\"\n\n{}", from, to, error)
     }
-  }
-
-  pub fn empty_dir(dir_path: &str) {
-    if self::exists(dir_path) {
-      fs::remove_dir_all(dir_path).expect(&format!("Failed to delete dir \"{}\"", dir_path));
-      fs::create_dir(dir_path).expect(&format!("Failed to create dir \"{}\"", dir_path));
-    }
-  }
-
-  pub fn ensure_dir(path: &str) {
-    if !self::exists(path) {
-      match fs::create_dir_all(path) {
-        Ok(_) => {},
-        Err(error) => panic!("Error: failed to create directory \"{}\"\n\n{}", path, error)
-      }
-    }
-  }
-
-  pub fn exists(path: &str) -> bool {
-    Path::new(path).exists()
   }
 }
 
-pub mod string {
-  use regex::Regex;
 
-  pub fn to_slug(value: &String) -> String {
-    let mut result: String = value.to_lowercase();
-    let non_alpha_numeric: Regex = Regex::new(r"[^a-zA-Z0-9]+").unwrap();
-    let slug_delimiter: Regex = Regex::new(r"[\s]+").unwrap();
-    result = non_alpha_numeric.replace_all(&result, " ").trim().to_string();
-    result = slug_delimiter.replace_all(&result, "-").to_string();
-    result
-  }
-
-  pub fn escape_html_quotes(html: &String) -> String {
-    html.replace("'", "&apos;").replace("\"", "&quot;")
+/// **Description**:
+///
+/// Checks if a given directory exists and creates it if it doesn't
+///
+/// **References**:
+/// * [Path::new(...).exists()](https://doc.rust-lang.org/stable/std/path/struct.Path.html#method.exists)
+/// * [fs::create_dir](https://doc.rust-lang.org/std/fs/fn.create_dir.html)
+pub fn ensure_dir(path: &str) {
+  if !self::exists(path) {
+    match fs::create_dir_all(path) {
+      Ok(_) => {},
+      Err(error) => panic!("Error: failed to create directory \"{}\"\n\n{}", path, error)
+    }
   }
 }
 
-pub mod walk {
-  use glob::glob;
 
-  pub fn dir(dir_path: &str) -> Vec<String> {
-    let error: &str = &format!("Error: failed to glob \"{}\"", dir_path);
-    glob(dir_path)
-      .expect(error)
-      .map(| path |
-        path.expect(error)
-            .display()
-            .to_string()
-      )
-      .collect::<Vec<String>>()
+/// **Description**:
+///
+/// Empties a specified directory
+///
+/// **Behaviour**:
+/// * Avoid not exists errors (`lib::exists`)
+///   * If the directory doesn't exist, it's created and the function exits
+/// * Deletes the directory after deleting it's contents (`fs::remove_dir_all`)
+/// * Create a new directory at the given path (`fs::create_dir`)
+///
+/// **Notes**:
+/// * Fails if user lacks permissions for the directory
+/// * Does not follow symbolic links it simply removes the link itself
+///
+/// **References**:
+/// * [fs::remove_dir_all](https://doc.rust-lang.org/std/fs/fn.remove_dir_all.html)
+/// * [fs::create_dir](https://doc.rust-lang.org/std/fs/fn.create_dir.html)
+/// * [Path::new(...).exists()](https://doc.rust-lang.org/stable/std/path/struct.Path.html#method.exists)
+pub fn empty_dir(dir_path: &str) {
+  let empty_error: &str = &*format!("Failed to empty dir \"{}\"", dir_path);
+  if !self::exists(dir_path) {
+    fs::create_dir(dir_path).expect(empty_error);
+  } else {
+    fs::remove_dir_all(dir_path).expect(empty_error);
+    fs::create_dir(dir_path).expect(empty_error);
   }
+}
+
+
+/// **Description**:
+///
+/// Glob a directory and return a Vector of Strings (wrapper for [glob::glob](https://docs.rs/glob/0.3.0/glob/fn.glob.html))
+///
+/// Where:
+///
+/// `lib::walk("./")`
+///
+/// Returns a Vec\<String\> containing the paths of each item in the current directory
+///
+/// Info: name inspired by [os.walk](https://docs.python.org/3/library/os.html#os.walk) in Python
+pub fn walk_dir(directory: &str) -> Vec<String> {
+  // Provide more information on panic
+  let glob_error: &str = &format!("Error: failed to glob directory \"{}\"", directory);
+  // Glob the given directory
+  glob(directory)
+    // Show the glob error on panic
+    .expect(glob_error)
+    // Convert each PathBuf into a String
+    .map(| path: Result<PathBuf, GlobError> | path.expect(glob_error).display().to_string())
+    // Collect the path String's into a Vector
+    .collect::<Vec<String>>()
+}
+
+
+/// **Description**:
+///
+/// Convert a string to slug case
+///
+/// **References**:
+/// * [What is a slug?](https://stackoverflow.com/a/19335586/10415695)
+pub fn string_to_slug(value: &String) -> String {
+  // remove all non alpha numeric values
+  let re_non_alpha_num: Regex = Regex::new(r"[^a-zA-Z0-9]+").unwrap();
+  let result = re_non_alpha_num.replace_all(&value.to_lowercase(), " ").to_string();
+  // replace all spaces with hyphens and trim
+  let re_delimiter: Regex = Regex::new(r"\s+").unwrap();
+  re_delimiter.replace_all(result.trim(), "-").to_string()
+}
+
+
+/// A list of characters and their corresponding HTML entities
+static HTML_ESCAPABLE: [[&str; 2]; 31] = [
+  [r"!", "&#33;"], // Exclamation mark
+  ["\"", "&#34;"], // Quotation mark
+  [r"#", "&#35;"], // Number sign
+  [r"$", "&#36;"], // Dollar sign
+  [r"%", "&#37;"], // Percent sign
+  [r"&", "&#38;"], // Ampersand
+  [r"'", "&#39;"], // Apostrophe
+  [r"(", "&#40;"], // Opening/Left Parenthesis
+  [r")", "&#41;"], // Closing/Right Parenthesis
+  [r"*", "&#42;"], // Asterisk
+  [r"+", "&#43;"], // Plus sign
+  [r",", "&#44;"], // Comma
+  [r"-", "&#45;"], // Hyphen
+  [r".", "&#46;"], // Period
+  [r":", "&#58;"], // Colon
+  [r";", "&#59;"], // Semicolon
+  [r"<", "&#60;"], // Less-than
+  [r"=", "&#61;"], // Equals sign
+  [r">", "&#62;"], // Greater than
+  [r"?", "&#63;"], // Question mark
+  [r"@", "&#64;"], // At sign
+  [r"[", "&#91;"], // Opening/Left square bracket
+  [r"\", "&#92;"], // Backslash
+  [r"]", "&#93;"], // Closing/Right square bracket
+  [r"^", "&#94;"], // Caret
+  [r"_", "&#95;"], // Underscore
+  [r"`", "&#96;"], // Grave accent
+  ["{", "&#123;"], // Opening/Left curly brace
+  ["|", "&#124;"], // Vertical bar
+  ["}", "&#125;"], // Closing/Right curly brace
+  ["~", "&#126;"]  // Tilde
+];
+
+
+/// **Description**:
+///
+/// Escapes and sanitizes a string of HTML using `lib::HTML_ESCAPABLE` to provide a list of values to escape
+///
+/// **References**:
+/// * [HTML Sanitization](https://en.wikipedia.org/wiki/HTML_sanitization)
+/// * [HTML Entity List](https://www.freeformatter.com/html-entities.html#misc-html-entities)
+pub fn escape_html(html: &String) -> String {
+  // Replace the characters with their matching HTML entities
+  let mut result: String = html.to_owned();
+  for [character, replacement] in self::HTML_ESCAPABLE {
+    result = result.replace(character, replacement);
+  }
+  result
 }
