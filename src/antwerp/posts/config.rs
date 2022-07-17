0252dd688274c6aa6ae14c172a2b566ccd6873cc
LogicalBranch <>
2022-07-17 21:55:52 +0100

    reorganise modules

--- src/antwerp/posts/config.rs
+++
@@ -1,7 +0,0 @@
-pub struct PostsConfig<'a> {
-  pub uri: &'a str,
-  pub uri_post: &'a str,
-  pub dir_dist: &'a str,
-  pub dir_templates: &'a str,
-  pub path_render: &'a str
-}
--- src/antwerp/posts/mod.rs
+++
@@ -1,6 +0,0 @@
-//! Exposes post config and management utilities
-mod config;
-mod post;
-
-pub use crate::antwerp::posts::config::*;
-pub use crate::antwerp::posts::post::*;
