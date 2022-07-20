806df4fae1325093870cb1afd57b58afb71b0f30
LogicalBranch <>
2022-07-20 17:46:28 +0100

    simplify 'empty_root' signature

--- src/antwerp/assets/utils.rs
+++ src/antwerp/assets/utils.rs
@@ -3,9 +3,9 @@ use crate::antwerp::{Config, Lib};
 /// **Description**:
 ///
 /// Empty the root folder (see `lib::empty_dir`)
-pub fn empty_root(config: &Config, root: &str) {
+pub fn empty_root(config: &Config) {
   // Log if verbose is enabled
-  Lib::log(config.verbose, "red", "Empty", "folder", root);
+  Lib::log(config.verbose, "red", "Empty", "folder", &config.dir_output);
   // Empty the directory
-  Lib::empty_dir(root);
+  Lib::empty_dir(&config.dir_output);
 }
