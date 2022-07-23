0f745a733131c94b6e0174176414467779457603
LogicalBranch <>
2022-07-23 03:38:08 +0100

    re-export public names using wildcard

--- src/antwerp/core/mod.rs
+++ src/antwerp/core/mod.rs
@@ -1,6 +1,8 @@
 //! Exposes Antwerp's main `Config` struct and init method
+mod clean;
 mod config;
 mod init;
 
+pub use crate::antwerp::core::clean::*;
 pub use crate::antwerp::core::config::*;
 pub use crate::antwerp::core::init::*;
--- src/antwerp/handlers/mod.rs
+++ src/antwerp/handlers/mod.rs
@@ -2,9 +2,7 @@
 mod assets;
 mod posts;
 mod templates;
-mod utils;
 
-pub use crate::antwerp::handlers::assets::{Asset, assets};
-pub use crate::antwerp::handlers::posts::Post;
-pub use crate::antwerp::handlers::templates::{Template, Route, Routes, tera, render, render_string, route, route_group};
-pub use crate::antwerp::handlers::utils::clean_build;
+pub use crate::antwerp::handlers::assets::*;
+pub use crate::antwerp::handlers::posts::*;
+pub use crate::antwerp::handlers::templates::*;
