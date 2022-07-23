7afb235b70ee21d4c280a132d0b8e2971ef84bf1
LogicalBranch <>
2022-07-23 23:31:14 +0100

    implement file auditor for Issue #21

--- src/antwerp/core/mod.rs
+++ src/antwerp/core/mod.rs
@@ -1,8 +1,10 @@
-//! Exposes Antwerp's main `Config` struct and init method
+//! Exposes Antwerp's `audit`, `build`, `clean` and `config` utilities
+mod audit;
+mod build;
 mod clean;
 mod config;
-mod init;
 
+pub use crate::antwerp::core::audit::*;
 pub use crate::antwerp::core::clean::*;
 pub use crate::antwerp::core::config::*;
-pub use crate::antwerp::core::init::*;
+pub use crate::antwerp::core::build::*;
