d133cac50925469706f11153b2f5627de0e61a54
Malekai <>
2022-08-14 04:05:59 +0100

    integrate handler methods into `Antwerp`

--- src/handlers/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Exposes enums and structs used to encapsulate site data & resources and functions used to copy, compile and render assets, stylesheets and templates.
-mod assets;
-mod posts;
-mod templates;
-
-pub use crate::handlers::assets::{Asset, assets};
-pub use crate::handlers::posts::Post;
-pub use crate::handlers::templates::{Route, Routes, Template, tera, render, render_string, route, route_group};
