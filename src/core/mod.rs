906db35ccbb140d500ddbff4417f126df628a542
Malekai <>
2022-08-20 22:55:42 +0100

    rename `core::build` to `core::antwerp`

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,7 +1,7 @@
-mod build;
+mod antwerp;
 mod config;
 mod posts;
 
-pub use crate::core::build::Antwerp;
+pub use crate::core::antwerp::Antwerp;
 pub use crate::core::config::Config;
 pub use crate::core::posts::Post;
