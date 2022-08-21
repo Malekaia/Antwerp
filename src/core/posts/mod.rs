84aceebdf9c8a45790063fb588291b7a76f7d706
Malekai <>
2022-08-21 14:37:24 +0100

    reduce internal dependencies for `Post`

--- src/core/posts/mod.rs
+++ src/core/posts/mod.rs
@@ -1,4 +1,4 @@
-mod support;
+mod methods;
 
 use crate::{Antwerp, Lib};
 use serde::Serialize;
@@ -30,9 +30,9 @@ pub struct Post {
 }
 
 impl Post {
-  /// Extracts user-defined data from a template to generate and populate a new build of Post
+  /// Extracts user-defined data from a template to generate and populate a new instance of Post
   fn properties(build: &Antwerp, file_path: &String) -> Post {
-    // Create a new build of Post, where all fields are instantiated to empty strings
+    // Create a new instance of Post, where all fields are instantiated to empty strings
     let mut post: Post = Post {
       title: String::new(),
       description: String::new(),
@@ -61,25 +61,25 @@ impl Post {
     let file_content: String = Lib::read_file(&file_path);
     let mut content: String = file_content.to_owned();
     // Extract header data
-    support::header_data(&build, &mut post, &file_path, &file_content, &mut content);
+    methods::header_data(&build, &mut post, &file_path, &file_content, &mut content);
     // Set options to defaults if not exists
-    support::header_defaults(&build, &mut post);
+    methods::header_defaults(&build, &mut post);
     // Generate the table of contents
-    post.table_of_contents = support::table_of_contents(&file_content, &mut content);
+    post.table_of_contents = methods::table_of_contents(&file_content, &mut content);
     // Generate the estimated read time for the post
-    post.estimated_read_time = support::estimated_read_time(&content);
+    post.estimated_read_time = methods::estimated_read_time(&content);
     // Add the rendered content to the post
     post.template_raw = content;
     // Generate HTML metadata for the post
-    post.metadata = support::metadata(&post);
+    post.metadata = methods::metadata(&post);
     // Create a slug string for the post title
     post.slug = Lib::string_to_slug(&post.title);
     // Generate a url for the post
-    post.url = support::post_url(&build, &mut post);
+    post.url = methods::post_url(&build, &mut post);
     // Create the render path string
-    post.render_path = support::render_path(&build, &mut post);
+    post.render_path = methods::render_path(&build, &mut post);
     // Generate the template path
-    post.template_path = support::template_path(&build, &file_path);
+    post.template_path = methods::template_path(&build, &file_path);
     // Return the posts object
     post
   }
@@ -87,7 +87,7 @@ impl Post {
   /// Create a Vector containing Post objects containing information for each post in a specified directory
   pub fn list(build: &Antwerp, sorter: fn(Vec<Post>) -> Vec<Post>) -> Vec<Post> {
     // Walk the given directory & generate a sorted list containing properties for each post
-    let fn_map = | file_path: &String | Post::properties(build, file_path);
-    sorter(Lib::walk_dir(&build.dir_posts).iter().map(fn_map).collect::<Vec<Post>>())
+    let fn_map = | file_path: &String | Post::properties(&build, file_path);
+    sorter(Lib::walk_dir(&build.config.dir_posts()).iter().map(fn_map).collect::<Vec<Post>>())
   }
 }
